1600308d505b1a457e471c49aad6776e7b64f9d8
Amy de Buitléir <amy@nualeargais.ie>
2022-12-04 19:19:08 +0000

    refresh

--- default.nix
+++ default.nix
@@ -3,4 +3,10 @@ let
 in
   pkgs.haskellPackages.developPackage {
     root = ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [
+          cabal-install
+          # list additional tools here
+        ]);
   }
